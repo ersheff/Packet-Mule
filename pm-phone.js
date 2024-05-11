15db0c48a1004ec05894a14a52205165c5aa91fa
Eric Sheffield <ersheff@gmail.com>
2024-05-11 15:52:15 -0400

    asdf

--- pm-phone.js
+++ pm-phone.js
@@ -1,9 +1,6 @@
 export function handlePhone(socket, phone) {
-  let xyz,
-    lastXyz = [0, 0, 0];
-  let abg,
-    lastAbg = [0, 0, 0];
-  let xyzabg, sentXyzabg;
+  let xyz, abg, xyzabg, sentXyzabg;
+  let lastXyz = [0, 0, 0];
 
   document.body.innerHTML = `<p>Accelerometer:</p>
   <div id="accel-data"></div>
@@ -37,19 +34,19 @@ export function handlePhone(socket, phone) {
       .then((permissionState) => {
         if (permissionState === "granted") {
           window.addEventListener("deviceorientation", (event) => {
-            [abg, lastAbg] = handleOrientation(event, abg, lastAbg);
+            abg = handleOrientation(event);
           });
         }
       })
       .catch(console.error);
   } else {
     window.addEventListener("deviceorientation", (event) => {
-      [abg, lastAbg] = handleOrientation(event, abg, lastAbg);
+      abg = handleOrientation(event);
     });
   }
 
   setInterval(() => {
-    xyzabg = [...xyz, ...abg.map((rad) => rad * 180 / Math.PI)];
+    xyzabg = [...xyz, ...abg];
     if (JSON.stringify(sentXyzabg) !== JSON.stringify(xyzabg)) {
       sentXyzabg = Array.from(xyzabg);
       socket.emit("phone", { target: phone, data: sentXyzabg });
@@ -72,13 +69,11 @@ function handleMotion(event, xyz, lastXyz) {
   return [xyz, lastXyz];
 }
 
-function handleOrientation(event, abg, lastAbg) {
-  const newAbg = [event.alpha, event.beta, event.gamma].map(deg => deg * Math.PI / 180);;
-  abg = lastAbg.map((v, i) => v * 0.85 + newAbg[i] * 0.15);
-  lastAbg = abg;
+function handleOrientation(event) {
+  const abg = [event.alpha, event.beta, event.gamma];
   document.getElementById("gyro-data").innerHTML = `
     <p>x: ${abg[0].toFixed(2)}</p>
     <p>x: ${abg[1].toFixed(2)}</p>
     <p>x: ${abg[2].toFixed(2)}</p>`;
-  return [abg, lastAbg];
+  return abg;
 }
