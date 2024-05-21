d1e2a5a8410ce115574a06d612aa3f88bfceb370
Eric Sheffield <ersheff@gmail.com>
2024-05-21 12:44:34 -0400

    testing

--- main.js
+++ main.js
@@ -1,50 +1,89 @@
 import { handlePhone } from "./pm-phone.js";
 import { handleMax } from "./pm-max.js";
+import { handleTest } from "./pm-test.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const params = new URLSearchParams(window.location.search);
-  const phone = params.get("phone");
-  const max = params.get("max");
-  const pass = params.get("pass");
+const params = new URLSearchParams(window.location.search);
+const phone = params.get("phone");
+const max = params.get("max");
+const pass = params.get("pass");
 
-  if (!window.max && !phone) {
-    document.body.innerHTML =
-      /* HTML */
-      `<div style="padding: 1rem; text-align: center;">
-        <h1 style="margin-top: 1rem;">Whatchoo doin'?</h1>
-        <p>
-          To use Packet Mule in Max, load this page in a jweb object with the
-          query string <code>?pass=password</code>.
-        </p>
-        <p>
-          To send controller data from your phone to Max, load this page in a
-          mobile browser with the query string
-          <code>?pass=password?phone=username</code>.
-        </p>
-      </div>`;
-    return;
+// if (!window.max && !phone) {
+//   document.body.innerHTML =
+//     /* HTML */
+//     `<div style="padding: 1rem; text-align: center;">
+//       <h1 style="margin-top: 1rem;">Whatchoo doin'?</h1>
+//       <p>
+//         To use Packet Mule in Max, load this page in a jweb object with the
+//         query string <code>?pass=password</code>.
+//       </p>
+//       <p>
+//         To send controller data from your phone to Max, load this page in a
+//         mobile browser with the query string
+//         <code>?pass=password?phone=username</code>.
+//       </p>
+//     </div>`;
+//   return;
+// }
+
+const socket = io({ auth: { token: pass } });
+
+socket.on("connect", () => {
+  if (socket.recovered) {
+    console.log("recovered");
+  } else {
+    handleConnect();
   }
+  setTimeout(() => {
+    // close the low-level connection and trigger a reconnection
+    socket.io.engine.close();
+  }, 5000);
+});
 
-  const socket = io({ auth: { token: pass } });
+function handleConnect() {
+  authAndHandlers(socket, phone, max);
 
-  socket.on("auth", (response) => {
+  socket.once("disconnect", () => {
+    console.log("disconnected");
+    console.log(socket);
+    // socket.removeAllListeners();
+    //socket.once("connect", handleConnect);
+  });
+}
+
+function authAndHandlers(socket, phone, max) {
+  socket.once("auth", (response) => {
     if (!response.success) {
       document.body.innerHTML = `<h1 style="text-align: center;">What's the password?</h1>`;
       return;
     }
 
     if (!window.max) {
-      handlePhone(socket, phone);
-      return;
+      // handlePhone(socket, phone);
+      document.body.innerHTML =
+        /* HTML */
+        `<dialog id="username-modal">
+          <form method="dialog">
+            <input
+              type="text"
+              id="username-input"
+              placeholder="enter username"
+            />
+          </form>
+        </dialog>`;
+      handleTest(socket);
+    } else {
+      document.body.innerHTML =
+        /* HTML */
+        `<dialog id="username-modal">
+          <form method="dialog">
+            <input
+              type="text"
+              id="username-input"
+              placeholder="enter username"
+            />
+          </form>
+        </dialog>`;
+      handleMax(socket, max);
     }
-
-    document.body.innerHTML =
-      /* HTML */
-      `<dialog>
-        <form method="dialog">
-          <input type="text" id="username-input" placeholder="enter username" />
-        </form>
-      </dialog>`;
-    handleMax(socket, max);
   });
-});
+}
--- server.js
+++ server.js
@@ -6,7 +6,13 @@ import bcrypt from "bcrypt";
 const app = express();
 app.use(express.static("./"));
 const server = createServer(app);
-const io = new Server(server);
+const disconnectTimeout = 11000;
+const io = new Server(server, {
+  connectionStateRecovery: {
+    maxDisconnectionDuration: disconnectTimeout,
+    skipMiddlewares: true
+  }
+});
 
 const port = process.env.PORT || 3000;
 const password = process.env.PM_PWD;
@@ -18,13 +24,108 @@ const users = {};
 const buffers = {};
 
 io.on("connection", (socket) => {
-  const token = socket.handshake.auth.token;
-  if (!token || !bcrypt.compareSync(token, hashedPassword)) {
-    socket.emit("auth", { success: false });
-    socket.disconnect();
-    return;
+  console.log(`connect ${socket.id}`);
+
+  if (socket.recovered && socket.data.reconnectTimeout) {
+    console.log("recovered");
+    clearTimeout(socket.data.reconnectTimeout);
+    socket.data.reconnectTimeout = null;
+    console.log(socket.data.reconnectTimeout);
+    console.log(users);
+  } else {
+    newConnection(socket);
   }
-  socket.emit("auth", { success: true });
+
+  // handle data from max
+  socket.on("pm", (incoming) => {
+    incoming.forEach((packet) => {
+      const { target } = packet;
+      const userId = users[target];
+      const _room = `_${target}`;
+      const _roomTarget = io.sockets.adapter.rooms.has(_room);
+
+      if (userId || _roomTarget || target === "all") {
+        const outgoing = {
+          source: _roomTarget ? target : socket.data.username,
+          header: packet.header,
+          data: packet.data
+        };
+        if (target === "all") {
+          socket.broadcast.emit("pm", outgoing);
+        } else if (_roomTarget) {
+          buffers[_room] = buffers[_room] || [];
+          buffers[_room].push(outgoing);
+        } else {
+          buffers[userId] = buffers[userId] || [];
+          buffers[userId].push(outgoing);
+        }
+      } else {
+        console.log(`No target found for ${target}.`);
+      }
+    });
+  });
+
+  // handle data from phone
+  socket.on("phone", (incoming) => {
+    const target = users[incoming.target];
+    socket.to(target).emit("phone", incoming.data);
+  });
+
+  socket.on("chat", (incoming) => {
+    console.log(`chat from ${socket.data.username}`);
+    const outgoing = `<p>${socket.data.username}: ${incoming}</p>`;
+    console.log("chat", outgoing);
+    io.emit("chat", outgoing);
+  });
+
+  socket.on("join-room", (room) => {
+    const _room = `_${room}`;
+    const _roomNew = !io.sockets.adapter.rooms.has(_room);
+    socket.join(_room);
+    if (_roomNew) {
+      updateRoomsAll();
+    } else updateRooms(socket);
+  });
+
+  socket.on("leave-room", (room) => {
+    const _room = `_${room}`;
+    socket.leave(_room);
+    const _roomEmpty = !io.sockets.adapter.rooms.has(_room);
+    if (_roomEmpty) {
+      updateRoomsAll();
+    } else updateRooms(socket);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(`disconnect ${socket.id} due to ${reason}`);
+    socket.data.reconnectTimeout = setTimeout(() => {
+      if (users[socket.data.username]) {
+        delete users[socket.data.username];
+        delete buffers[socket.id];
+        updateUsers();
+        const _rooms = Array.from(socket.rooms).filter((room) =>
+          room.startsWith("_")
+        );
+        for (const _room of _rooms) {
+          if (io.sockets.adapter.rooms.get(_room).size === 1) {
+            updateRoomsAll();
+            break;
+          }
+        }
+        console.log("user data deleted for ", socket.data.username);
+      }
+    }, disconnectTimeout - 1000);
+  });
+});
+
+function newConnection(socket) {
+  if (password) {
+    const token = socket.handshake.auth.token;
+    if (!token || !bcrypt.compareSync(token, hashedPassword)) {
+      socket.emit("auth", { success: false });
+      socket.disconnect();
+    } else socket.emit("auth", { success: true });
+  }
 
   // set username
   socket.on("username", (username) => {
@@ -36,51 +137,56 @@ io.on("connection", (socket) => {
     users[socket.data.username] = socket.id;
     socket.data.username = username;
     socket.emit("username", { success: true, username: socket.data.username });
+    updateUsers();
+    updateRooms(socket);
   });
+}
 
-  // handle data from max
-  socket.on("pm", (incoming) => {
-    for (const packet of incoming) {
-      const target = users[packet.target];
-      if (!target && packet.target !== "all") {
-        return console.log("Target not found");
-      }
-      const outgoing = {
-        sender: socket.data.username,
-        header: packet.header,
-        data: packet.data
-      };
-      if (packet.target === "all") {
-        socket.broadcast.emit("pm", [outgoing]);
-      } else {
-        buffers[target] = buffers[target] || [];
-        buffers[target].push(outgoing);
-      }
-    }
-  });
+//
 
-  // handle data from phone
-  socket.on("phone", (incoming) => {
-    const target = users[incoming.target];
-    socket.to(target).emit("phone", incoming.data);
+function updateUsers() {
+  const userArray = Object.keys(users);
+  let userlist = `<p>Connected users: ${userArray.length}</p><ul>`;
+  userArray.forEach((user) => {
+    userlist += `<li>${user}</li>`;
   });
+  userlist += "</ul>";
+  io.emit("userlist", userlist);
+}
 
-  socket.on("chat", (incoming) => {
-    const outgoing = `<p>${socket.data.username}: ${incoming}</p>`;
-    io.emit("chat", outgoing);
+function updateRooms(socket) {
+  const _rooms = Array.from(io.sockets.adapter.rooms.keys()).filter((room) =>
+    room.startsWith("_")
+  );
+  let roomlist = `
+        <input type="text" id="room-input" placeholder="create group" />
+        <div class="spacer"></div>
+        <ul>
+    `;
+  _rooms.forEach((_room) => {
+    const isChecked = socket.rooms.has(_room) ? "checked" : "";
+    roomlist +=
+      /* HTML */
+      ` <li>
+        <input type="checkbox" value="${_room.substring(1)}" ${isChecked} />
+        <span>${_room.substring(1)}</span>
+      </li>`;
   });
+  roomlist += "</ul>";
+  socket.emit("roomlist", roomlist);
+}
 
-  socket.on("disconnect", () => {
-    delete users[socket.data.username];
-    delete buffers[socket.id];
+function updateRoomsAll() {
+  io.of("/").sockets.forEach((socket) => {
+    updateRooms(socket);
   });
-});
+}
 
 setInterval(() => {
-  for (const user in buffers) {
-    if (buffers[user].length > 0) {
-      io.to(user).emit("pm", buffers[user]);
-      buffers[user] = [];
+  for (const target in buffers) {
+    if (buffers[target].length > 0) {
+      io.to(target).emit("pm", buffers[target]);
+      buffers[target] = [];
     }
   }
 }, 50);
